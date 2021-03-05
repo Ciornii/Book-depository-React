4c5b82cb3b11250a9a6154beaf19881bc1ab7ad2
Victor Ciornii <ciorniivictor96@gmail.com>
2021-03-05 19:29:26 +0200

    Update book-list-item.js

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -2,11 +2,29 @@ import React from 'react';
 import './book-list-item.scss';
 
 const BookListItem = ({ book }) => {
-  const { title, author } = book;
+  const { title, author, description, photo } = book;
   return (
     <>
-      <span>{title}</span>
-      <span>{author}</span>
+      <div className='product__img'>
+        <img src={photo} alt={title} />
+      </div>
+      <div className='product__bottom'>
+        <div className='product__info'>
+          <div className='product__title'>{title}</div>
+          <div className='product__author'>by {author}</div>
+          <div className='product__description'>{description}</div>
+          <div className='product__read-more'>...</div>
+        </div>
+        <div className='product__actions'>
+          <a href='#' className='product__link'>
+            Quick View
+          </a>
+          <div className='product__btns'>
+            <button data-add-to='My Books' title='My Books'></button>
+            <button data-add-to='Wish List' title='Wish List'></button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
