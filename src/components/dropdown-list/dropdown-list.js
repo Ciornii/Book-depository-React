9886a9201c889899880264e219cf446614fe182b
Victor Ciornii <ciorniivictor96@gmail.com>
2021-06-20 21:46:28 +0300

    dropdown list

---
+++ src/components/dropdown-list/dropdown-list.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { bookRemovedFromMyList } from '../../actions';
+
+const DropdownList = ({ items, onDelete, active, title, close }) => {
+  const renderRow = (item) => {
+    const { id, title, author } = item;
+
+    return (
+      <li key={id} className="popup-list__item">
+        <div className="popup-list__title">{title}</div>
+        <div className="popup-list__author">by {author}</div>
+        <button className="popup-list__delete" onClick={() => onDelete(id)}>
+          x
+        </button>
+      </li>
+    );
+  };
+
+  return (
+    <div className={`popup-list ${active && 'active'}`}>
+      <div className='popup-list__main-title'>{title}</div>
+      <div className='popup-list__close' onClick={() => close(false)}>x</div>
+      <ul className='popup-list__wrapper'>{items.map(renderRow)}</ul>
+      <button className='popup-list__more'>View Full List</button>
+      <div className='popup-list__triangle'></div>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ myListItems: { myListItems } }) => {
+  return {
+    items: myListItems,
+  };
+};
+
+const mapDispatchToProps = {
+  onDelete: bookRemovedFromMyList,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DropdownList);
